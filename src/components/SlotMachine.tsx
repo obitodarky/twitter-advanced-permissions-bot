bb39fe57c0e713d55321b196d5e6fbc466d12c67
Aashish <aashishmethani5@gmail.com>
2026-01-01 08:17:36 +0530

    clean up

--- src/components/SlotMachine.tsx
+++ src/components/SlotMachine.tsx
@@ -91,21 +91,6 @@ const SlotMachine: React.FC<SlotMachineProps> = ({ className }) => {
               height={800}
             />
           </EffectComposer>
-          {/* <EffectComposer>
-            <DepthOfField
-              focusDistance={0}
-              focalLength={0.9}
-              bokehScale={2}
-              height={480}
-            />
-            <Bloom
-              luminanceThreshold={0}
-              luminanceSmoothing={0.9}
-              height={300}
-            />
-            <Noise opacity={0.02} />
-            <Vignette eskil={false} offset={0.1} darkness={1.1} />
-          </EffectComposer> */}
           <Preload all />
         </Canvas>
       </div>
